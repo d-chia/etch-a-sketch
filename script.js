7fad92d9b89f2ed052367f2ead7ec8a9f7c1304b
David C <d.chia@proton.me>
2022-07-22 02:00:45 +0200

    Add header and h1

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const container = document.querySelector(".container");
-const cell = document.createElement("div");
-cell.classList.add("cell");
 
 for (let i = 0; i < 16; i++) {
     const cell = document.createElement("div");
