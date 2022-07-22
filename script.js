8c57eec7c25aeb09ff4ff45e1db35d1d9b88fad8
David C <d.chia@proton.me>
2022-07-22 03:27:43 +0200

    Add eventlistener to all cells and change color on hover

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const container = document.querySelector(".container");
 
+
+//create 16 divs inside container div
+
 for (let i = 0; i < 16; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     container.appendChild(cell);
 }
+
+//add event listener to all cells for mouseenter
+
+const cellGrid = document.querySelectorAll(".cell");
+
+cellGrid.forEach((cell) => {
+    cell.addEventListener("mouseenter", paintCell);
+})
+
+
+//declare function that changes class of "painted" cells 
+
+function paintCell() {
+  this.classList.add("cellPainted");
+};
